c4226d48998073192fa435a92c935164030eba95
CowBoys1st <draharjo9@gmail.com>
2024-06-27 19:31:52 +0700

    exercise day 3

---
+++ module_03/exercise/convert.js
@@ -0,0 +1,4 @@
+let cm = 100
+let km = cm / 100000
+
+console.log(`${cm} Cm sama dengan ${km} Km`)
---
+++ module_03/exercise/multiplication.js
@@ -0,0 +1,10 @@
+// membuat tabel perkalian
+
+// memasukkan variabel
+let int = 9
+let result = 0
+for (i = 1; i <=10; i++) {
+    result = int * i
+    console.log(`9 X ${i} = ${result}`)
+}
+
