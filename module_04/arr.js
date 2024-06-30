b7bc955a6d9e814a3b80422be77e0dcab0b96a64
CowBoys1st <draharjo9@gmail.com>
2024-07-01 00:22:42 +0700

    apalah ini

---
+++ module_04/arr.js
@@ -0,0 +1,21 @@
+let arr = [1,2,3,4,5]
+let arr2 = [1,2,3,4,5]
+
+// for ()
+
+num.splice(1,2) // menghapus ditengah
+num
+
+let num = [1, 10, 89, 7, 2, 6, 3, 4, 5]
+num.sort((a, b) => b - a) // descending
+num.sort((a, b) => a - b) // ascending
+
+num.reverse()
+
+console.log(num)
+
+const fruits = ["apple", "banana", "melon"]
+
+for ( let fruit of fruits) {
+    console.log(fruit)
+}
