cf316f7f8ee1997535051679077a190c07752bbb
CowBoys1st <draharjo9@gmail.com>
2024-07-02 18:09:23 +0700

    exercise day-4

--- exercise/minMaxAverage.js
+++ exercise/minMaxAverage.js
@@ -1,4 +1,20 @@
-var array = [12, 5, 23, 18, 4, 45, 32]
-var sum = sum + array
+function statistikSort(arr) {
+    if (arr.length === 0) return null
 
-console.log(sum)
+    arr.sort((a, b) => a - b)
+
+    const min = arr[0]
+    const max = arr[arr.length - 1]
+
+    const sum = arr.reduce((jumlah, nilai) => jumlah + nilai, 0)
+    const avg = sum / arr.length
+
+    return min
+}
+
+
+
+const arr = [12, 5, 23, 18, 4, 45, 32]
+const res1 = statistikSort(arr)
+
+console.log(res1)
